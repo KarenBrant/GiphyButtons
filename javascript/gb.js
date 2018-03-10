bad5920b409c641d9e837d54bc8b04a79919acac
Karen Brant <karen.brant3@gmail.com>
2018-03-10 09:44:30 -0500

    buttons don't work the 2nd time

--- javascript/gb.js
+++ javascript/gb.js
@@ -1,6 +1,11 @@
 var el;
+var still;
+var gifArr = ["batman","wonder woman"];
+
 function getGifs() {
-    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=Fa70mFCWf8G52I9zVzhUHWRb20hnGjjM&limit=10&q=batman&rating=G";
+    var eachGif = $(this).attr("data-name");
+    console.log("eachGif: " + eachGif);
+    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=Fa70mFCWf8G52I9zVzhUHWRb20hnGjjM&limit=10&q=" + eachGif + "&rating=G";
 
     $.ajax( {
         url: queryURL,
@@ -9,13 +14,13 @@ function getGifs() {
           var gifs = response.data;
           console.log(gifs);
           for (var i=0; i<gifs.length;i++) {
-            //   $('#gifs').append('<div class="gif">' + '<img src="' + gifs[i].images.downsized.url + '">' + '</div>')
+                console.log("length: " + gifs.length);
                 var gif = gifs[i];
                 var image = gif.images.downsized.url;
+                console.log(gif);
                 el = $('<div class="gif">' + '<img src="' + image + '">' + '</div>')
-                var still = gif.images.downsized_still.url;
+                still = gif.images.downsized_still.url;
                 $('#gifs').append(el);
-                
             }  
         });
     }
@@ -34,5 +39,29 @@ function getGifs() {
                 $(this).css('background-image', 'url(' +$(this).data("animated") + ')');
             }
     }
-    getGifs(); 
-    toggleGif();  
+    
+    function getButtons() {
+        $("#buttons-view").empty();
+        for (var i=0; i<gifArr.length; i++) {
+            var btn=$("<button>");
+            btn.addClass("gif-btn");
+            btn.attr("data-name", gifArr[i]);
+            btn.text(gifArr[i]);
+            $("#buttons-view").append(btn);
+        }
+    }
+
+    $("#add-gif").on("click", function(event){
+        event.preventDefault();
+        var gifAdd = $("#gif-input").val().trim();
+        gifArr.push(gifAdd);
+        getButtons();
+        console.log("added to array");
+    });
+
+    $(document).on("click",".gif-btn", getGifs);
+    console.log("when does this happen");
+
+    getButtons();
+    // getGifs(); 
+    // toggleGif();  
