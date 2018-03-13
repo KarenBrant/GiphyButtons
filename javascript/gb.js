3eb73c71fe360c5c3a876bf558e4a512c4686953
Karen Brant <karen.brant3@gmail.com>
2018-03-12 21:48:30 -0400

    Final commit #2

--- javascript/gb.js
+++ javascript/gb.js
@@ -1,10 +1,11 @@
-
+// Array of 2 superheroes to start
 var topics = ["batman","wonder woman"];
 
+// Function to get the gifs to display
 function getGifs() {
     var eachGif = $(this).attr("data-name");
     console.log("eachGif: " + eachGif);
-    var queryURL = "http://api.giphy.com/v1/gifs/search?api_key=Fa70mFCWf8G52I9zVzhUHWRb20hnGjjM&limit=10&q=" + eachGif + "&rating=G";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=Fa70mFCWf8G52I9zVzhUHWRb20hnGjjM&limit=10&q=" + eachGif + "&rating=G";
 
     $.ajax( {
         url: queryURL,
@@ -13,17 +14,20 @@ function getGifs() {
           var gifs = response.data;
           console.log(gifs);
           $('#gifs').empty();
+          
+        //  Loop through to get the gifs
           for (var i=0; i<gifs.length;i++) {
-                console.log("length: " + gifs.length);
                 var gif = gifs[i];
+                // Create variables for animated gifs and still gifs
                 var animated = gif.images.downsized.url;
                 var still = gif.images.downsized_still.url;
+                // Get the rating for each gif
                 var rating = gif.rating;
-                // console.log(animated);
+                // Create the still gifs and add the rating to the html
                 var el = $('<div class="image" style="background-image: url(' + still + ')"></div>');
                 var rate = $('<p id="rating" class="text-center">').text("Rating: " + rating);
                 el.append(rate);
-        
+                // Set the data attributes
                 el.data('active', false);
                 el.data('still', still);
                 el.data('animated', animated);
@@ -31,7 +35,8 @@ function getGifs() {
             }  
         });
     }
-    
+
+    // Function to toggle from animated to still with clicks
     function toggleGif() {
         console.log("toggleGif");
         if ($(this).data('active')) {
@@ -42,31 +47,34 @@ function getGifs() {
             $(this).css('background-image', 'url(' +$(this).data("animated") + ')');
         }
     }
+
+    // Check for clicks on the gifs
     $(document).on('click', '.image', toggleGif);
     
+    // Function to create buttons for everything in the array
     function getButtons() {
         $("#buttons-view").empty();
         for (var i=0; i<topics.length; i++) {
             var btn=$('<button>');
             btn.addClass("gif-btn btn btn-primary");
-            // btn.addClass("btn");
-            // btn.addClass("btn-success");
             btn.attr("data-name", topics[i]);
             btn.text(topics[i]);
             $("#buttons-view").append(btn);
         }
     }
 
+    // Function to add additional items to the array
     $("#add-gif").on("click", function(event){
         event.preventDefault();
         var gifAdd = $("#gif-input").val().trim();
         topics.push(gifAdd);
         getButtons();
         console.log("added to array");
-        // $('#gif-input').empty();
     });
     
+    // Check for button clicks to call the getGifs function
     $(document).on("click",".gif-btn", getGifs);
     console.log("when does this happen");
 
+    // Call the getButtons function
     getButtons();
